e0a960a16fbb295202a4f0947119fa459c0be502
Matyas Gombos <gombosmm@gmail.com>
2023-01-09 17:31:02 -0500

    Add student and teacher class

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require './person'
+
+# teacher class derived from person
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown')
+    super(name, age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end

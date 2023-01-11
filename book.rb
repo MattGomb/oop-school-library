c80bc61576719449e7f206cb09b17cb4085d32c9
Matyas Gombos <gombosmm@gmail.com>
2023-01-11 14:22:20 -0500

    Fix first linter errors

--- book.rb
+++ book.rb
@@ -1,6 +1,7 @@
-#class for book to be rented to students
+# class for book to be rented to students
 class Book
   attr_accessor :title, :author
+
   def initialize(title, author)
     @title = title
     @author = author
--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,7 @@
-#class to assign students to a classroom
+# class to assign students to a classroom
 class Classroom
   attr_accessor :label
+
   def initialize(label)
     @label = label
   end

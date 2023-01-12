0de1934daf9e9ce8eb7935b1e4e796526b110726
Matyas Gombos <gombosmm@gmail.com>
2023-01-12 10:21:21 -0500

    Create app and main and insert the plans

---
+++ app.rb
@@ -0,0 +1,9 @@
+#class to have basic UI
+class App
+  # list all books
+  # List all people.
+  # Create a person (teacher or student, not a plain Person).
+  # Create a book.
+  # Create a rental.
+  # List all rentals for a given person id.
+end 
---
+++ main.rb
@@ -0,0 +1,7 @@
+# call to have an entry point to the app
+class Main
+  # Present the user with a list of options to perform.
+  # Lets users choose an option.
+  # If needed, ask for parameters for the option.
+  # Have a way to quit the app.
+end

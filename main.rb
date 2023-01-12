b7c75e4124280d0f8b7bc9daf4df0731ad874fbe
Matyas Gombos <gombosmm@gmail.com>
2023-01-12 16:44:00 -0500

    Connect app and main files

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative './app'
 # class to have an entry point to the app
 class Interface
   puts 'Welcome to the School Library App'
-  def Interface
+  def interface
     puts 'Please choose an option by entering a number:'
     puts '1 - List all books'
     puts '2 - List all people'
@@ -18,9 +18,34 @@ end
 class Main
   def initialize
     @app = App.new
-    @Interface = Interface.new
+    @interface = Interface.new
   end
- 
-end
 
-main()
+  def run
+    puts @interface.Interface
+    input = gets.chomp.to_i
+    options(input)
+  end
+
+  def options(input)
+    selection = {
+      1 => 'list_books',
+      2 => 'list_people',
+      3 => 'create_person',
+      4 => 'create_book',
+      5 => 'create_rental',
+      6 => 'list_rentals'
+    }
+    case input
+    when 1..6
+      @app.send(selection[input])
+    when 7
+      puts 'Thank you for using this app!'
+    else
+      puts 'Invalid option, please type 1, 2, 3, 4, 5, 6 or 7'
+      run
+    end
+  end
+end
+main = Main.new
+main.run

b359e5e6e62b525fcc7ea2baabd2bacb1e15e9d8
Matyas Gombos <gombosmm@gmail.com>
2023-01-12 15:22:54 -0500

    Prepare interface in main and inital app structure

--- app.rb
+++ app.rb
@@ -6,4 +6,63 @@ class App
   # Create a book.
   # Create a rental.
   # List all rentals for a given person id.
-end 
+  def app
+    puts 'Welcome to the School Library App!'
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    input = gets.chomp
+    case input
+    when '1'
+      puts 'List of books'
+    when '2'
+      puts 'List of people'
+    when '3'
+      puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+      print input 
+      if input == '1'
+        puts 'Age:'
+        print input
+        puts 'Name:'
+        print input
+        puts 'Has parent permission? [Y/N]:'
+        print input
+          unless input == 'Y' || 'N' || 'y' || 'n'
+            puts 'Invalid input: please enter Y or N'
+        puts 'Student created successfully'
+      elsif input == '2'
+        puts 'Age:'
+        print input
+        puts 'Name:'
+        print input
+        puts 'Specialization:'
+        print input
+        puts 'Teacher created successfully'
+      else
+        puts 'Invalid input: please enter 1 or 2'
+      end
+    when '4'
+      puts 'Title: '
+      print input
+      puts 'Author: '
+      print input
+      puts 'Book created successfully'
+    when '5'
+      puts 'Select a book from the following list by number'
+    when '6'
+      puts 'List all rentals for a given person id'
+    when '7'
+      puts 'Exit'
+    else
+      puts 'Invalid option'
+    end
+  end
+
+  app()
+end
+ 
--- main.rb
+++ main.rb
@@ -1,7 +1,26 @@
-# call to have an entry point to the app
-class Main
-  # Present the user with a list of options to perform.
-  # Lets users choose an option.
-  # If needed, ask for parameters for the option.
-  # Have a way to quit the app.
+require_relative './app'
+# class to have an entry point to the app
+class Interface
+  puts 'Welcome to the School Library App'
+  def Interface
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
 end
+
+# class to take the functionality of the menu
+class Main
+  def initialize
+    @app = App.new
+    @Interface = Interface.new
+  end
+ 
+end
+
+main()

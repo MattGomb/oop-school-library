7e1b777d083dc3d1519efdb1a52dbfee5b324bb2
Matyas Gombos <gombosmm@gmail.com>
2023-01-12 15:48:48 -0500

    Add create person method

--- app.rb
+++ app.rb
@@ -30,6 +30,36 @@ class App
   end
 
   def create_person
+    print 'Do you want to create a student (1) or a Teacher (2)? [Input the number]: '
+    select = gets.chomp.to_i
+    if[1, 2].include?(select)
+      print 'Age: '
+      age = gets.chomp
+      print 'Name: '
+      name = gets.chomp
+      case select
+      when 1 
+        print 'Classroom: '
+        classroom = gets.chomp
+        print 'Has parent permission? [Y/N]: '
+        permission = gets.chomp
+        unless permission == 'Y' || 'N' || 'y' || 'n'
+          puts 'Invalid input, please write Y or N'
+          print 'Has parent permission? [Y/N]: '
+          permission = gets.chomp
+        end
+        Student.new(age, classroom, name: name, parent_permission: permission)
+        puts 'Student added successfully!'
+      when 2
+        print 'Specialization: '
+        specialization = gets.chomp
+        Teacher.new(age, specialization, name: name)
+        puts 'Teacher added successfully!'
+      end
+    else
+      puts 'Invalid option'
+      return
+    end
   end
 
   def create_book
